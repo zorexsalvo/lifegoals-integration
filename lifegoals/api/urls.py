757476f775ef9970b8e34f5dd8c3d39e8d6512e2
ZorexSalvo <zsalvo@ayannah.com>
2016-08-27 18:28:14 +0800

    Create url endpoints
    These are the urls of the api endpoints.
    
    Signed-off-by: ZorexSalvo <zsalvo@ayannah.com>

---
+++ lifegoals/api/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+from api import views
+
+urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^accounts/$', views.WalletDetail.as_view(), name='wallet'),
+    url(r'^transfers/$', views.FundTransfer.as_view(), name='fund_transfer')
+]
