2130d79e909ad45282ecf5e0cd812398b5fb7e88
ZorexSalvo <zsalvo@ayannah.com>
2016-08-27 18:26:07 +0800

    Create the the accounts/ endpoint
    This view accepts a POST data ['access_token'], then
    returns the PBTC Wallet of that user.
    
    Signed-off-by: ZorexSalvo <zsalvo@ayannah.com>

--- lifegoals/api/views.py
+++ lifegoals/api/views.py
@@ -1,3 +1,29 @@
+from api.serializers import WalletSerializer
 from django.shortcuts import render
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import json
+import logging
+import requests
 
-# Create your views here.
+
+class WalletDetail(APIView):
+    def post(self, request):
+        access_token = request.data['access_token']
+        type = request.data['type']
+
+        if type == 'COINS':
+            url = 'https://coins.ph/api/v3/crypto-accounts'
+            headers = {'Authorization': 'Bearer {}'.format(access_token)}
+
+            r = requests.get(url, headers=headers)
+            try:
+                wallet_detail = r.json()['crypto-accounts'][0]
+                serializer = WalletSerializer(wallet_detail)
+                return Response(serializer.data)
+            except KeyError as e:
+                logging.error(str(e))
+                return Response(r.json())
+
+
