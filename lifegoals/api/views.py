613e669d6fec3466be211242ab53237c17bf3a86
ZorexSalvo <zsalvo@ayannah.com>
2016-08-27 18:27:35 +0800

    Create the transfers/ endpoint
    This is the view wherein you can do the
    fund transfer.
    
    Signed-off-by: ZorexSalvo <zsalvo@ayannah.com>

--- lifegoals/api/views.py
+++ lifegoals/api/views.py
@@ -27,3 +27,42 @@ class WalletDetail(APIView):
                 return Response(r.json())
 
 
+class FundTransfer(APIView):
+
+    def get_account(self, access_token):
+        url = 'https://coins.ph/api/v3/crypto-accounts'
+        headers = {'Authorization': 'Bearer {}'.format(access_token)}
+
+        r = requests.get(url, headers=headers)
+        try:
+            wallet_detail = r.json()['crypto-accounts'][0]
+            logging.error(wallet_detail)
+            logging.error(wallet_detail.get('id'))
+            return wallet_detail.get('id')
+        except KeyError:
+            logging.error('Account ID not found.')
+
+    def post(self, request):
+        access_token = request.data['access_token']
+        amount = request.data['amount']
+        target_address = request.data['target_address']
+        type = request.data['type']
+
+        url = 'https://coins.ph/api/v3/transfers/'
+        headers = {
+                    'Authorization': 'Bearer {}'.format(access_token),
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    'Accept': 'application/json'
+                  }
+
+        if type == 'COINS':
+            account = self.get_account(access_token)
+            logging.error(account)
+            body = json.dumps({
+                                "account": account,
+                                "target_address": target_address,
+                                "amount": amount
+                             })
+            r = requests.post(url, data=body, headers=headers)
+            return Response(r.json())
+        raise Http404body
